f9537a87700ab91cdc55fae1028ceffbe9e6d708
z64 <zachnowicki@gmail.com>
2017-04-12 23:28:26 -0400

    add basic logging app

--- src/traikoa.cr
+++ src/traikoa.cr
@@ -1,4 +1,15 @@
 require "./traikoa/*"
 
 module Traikoa
+  # Set up a fiber to listen to the relay dispatches
+  relay_channel = Channel(String).new
+  spawn do
+    loop do
+      relay_payload = relay_channel.receive
+      EDDN::LOGGER.info EDDN.parse_message(relay_payload).inspect
+    end
+  end
+
+  # Establish a connection and start handling dispatches
+  EDDN::Client.new.run(channel)
 end
