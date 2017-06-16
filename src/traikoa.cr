ed2673698907ed1fd36b716e853739d594bb3116
Zac Nowicki <zachnowicki@gmail.com>
2017-06-16 00:18:06 -0400

    no reason not to block here for now without sleeping the fiber

--- src/traikoa.cr
+++ src/traikoa.cr
@@ -5,18 +5,13 @@ module Traikoa
 
   relay_channel = client.run!
 
-  # Set up a fiber to listen to the relay dispatches
-  spawn do
-    loop do
-      relay_payload = relay_channel.receive
-      packet = EDDN.parse_message(relay_payload)
-      begin
-        EDDN::LOGGER.info packet.read_event
-      rescue
-        EDDN::LOGGER.info "(unsupported packet)"
-      end
+  loop do
+    relay_payload = relay_channel.receive
+    packet = EDDN.parse_message(relay_payload)
+    begin
+      EDDN::LOGGER.info packet.read_event
+    rescue
+      EDDN::LOGGER.info "(unsupported packet)"
     end
   end
-
-  sleep
 end
